8deb515155674d4590637f9e8819ab0399a00953
pankajrana <rpankaj853@gmail.com>
2021-03-16 18:14:51 +0530

    ecart project first commit

---
+++ aboutme.php
@@ -0,0 +1,98 @@
+
+<section id="section2">
+            <title>about me </title> 
+            <h1 class="display-4  text-center font-weight-bold mt-2 pt-5 wow fadeInDown"><span class="hr">  About</span></h1>
+            <div class="container-fluid pt-5">  
+            <div class="row">
+                <div class="col-6 col-md-3 d-flex justify-content-center ">
+                <figure class="figure">
+                   <i class="fa fa-rocket bg-info text-white rounded-circle fa-5x p-4 wow flipInY  " aria-hidden="true"></i>
+                    <figcaption class="figure-caption text-center">Dynamic </figcaption>
+                    
+                    </figure>
+                </div>
+                <div class="col-6 col-md-3 d-flex justify-content-center">
+                <figure class="figure">
+               <i class="fa fa-tachometer bg-info text-white fa-5x p-4 rounded-circle fa-5x wow flipInY " aria-hidden="true"></i>
+                    <figcaption class="figure-caption text-center ">Fast  </figcaption>
+                    
+                    </figure>
+                </div>
+               
+                <div class="col-6 col-md-3 d-flex justify-content-center ">
+                <figure class="figure">
+                 <i class="fa fa-laptop bg-info text-white fa-5x p-4 rounded-circle wow flipInY " aria-hidden="true"></i>
+                    <figcaption class="figure-caption text-center">Responsive</figcaption> 
+                    </figure>
+                 
+                </div>
+                <div class="col-6 col-md-3 d-flex justify-content-center ">
+                <figure class="figure">
+                 <i class="fa fa-home bg-info text-white fa-5x p-4 rounded-circle wow flipInY " aria-hidden="true"></i>
+
+                    <figcaption class="figure-caption text-center">Intuitive</figcaption> 
+                    </figure>
+                 
+                </div>
+                </div>
+            </div>
+            <!-- images -->
+            <div class="container pt-5">
+            <div class="row">
+                 <div class="col-sm-6 ">
+                   <figure class="figure wow fadeInLeft delay">
+                      <img src="mypic2.png"\ class="figure-img dp img-fluid pl-3 rounded  " >
+                      <figcaption class="figure-caption">Who's this guy?</figcaption>
+                       <p> A full stack developer currently pursuing Master in computer Application. I am very passionate when it comes to developing
+                       responsive website animation and creating an intuitive and dynamic user interface experience.
+                       Let's make something special.</p>
+                    </figure>
+
+                </div>
+                <!-------------------------------------- right side------------------------------------------------------>
+                <div class="col-sm-6">
+                   
+                    <div class="d-flex flex-column wow fadeInRight delay-1s ">
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">HTML5</p>
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">CSS3</p>
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">BOOTSTRAP</p>
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">JAVASCRIPT / JQUERY</p>
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">PHP</p>
+                    <p class="bg-info text-white text-center p-2 rounded flex-fill font-weight-bold">PYTHON / DJANGO</p>
+                    
+                    
+                    </div>
+             </div>           
+                </div>
+    
+            </div>
+            <div class="container pt-3 wow fadeInUp">
+            <h2 class="text-center font-weight-bold display-4"> Certificate </h2>
+                <div class="owl-carousel owl-theme pt-4 ">
+                <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_1.jpg">          
+                </div>
+                    <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_2.jpg">          
+                </div>
+                    <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_3.jpg">          
+                </div>
+                    <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_4.jpg">          
+                </div>
+                    <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_5.jpg">          
+                </div>
+                    <div class="item">
+                <img class="img-fluid" src="Certificate/Certificate/bca-certificate_6.jpg">          
+                </div>
+                
+                
+            </div>
+            </div>
+         
+
+    
+            
+        </section>
